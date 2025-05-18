a75a718b7d43e562c83c8fc003db0b2d621352c4
Alaa AL-Hhattami <140850528+AlaaAl-Hattami@users.noreply.github.com>
2025-05-18 17:01:01 +0300

    Fix typo in Cloudinary configuration variable name

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,7 +1,7 @@
 const { v2: cloudinary } = require("cloudinary");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUS_NAME,
+  cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
