83b19c9ea4e32c7ade8ee68783a9ffcc3210f579
Alaa AL-Hhattami <140850528+AlaaAl-Hattami@users.noreply.github.com>
2025-06-28 16:27:15 +0300

    Refactor route and update link: Remove unnecessary lines in addroute.js and change signup link to register in welcom.ejs

--- routes/addroute.js
+++ routes/addroute.js
@@ -3,8 +3,6 @@ const router = express.Router()
  const userManagementController = require("../Controllers/userManagementController")
  const {requierdAuth} =require("../middleware/middleware")
 
-
-
 // get طلبات
 router.get("/user/add.html", requierdAuth, userManagementController.user_add_get)
 
